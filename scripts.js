48cccbbfad6884021bd90e0762ab537dcf63f171
akhil <akhilmpattazhy@gmail.com>
2022-03-02 11:37:33 +0530

    render images in index.html , scripts.js modified

--- scripts.js
+++ scripts.js
@@ -1,14 +1,24 @@
 // fetch api
 let url = 'https://jsonplaceholder.typicode.com/photos/'
-let image = document.getElementById("image-card")
+let arr = [];
+let i = 0;
+let cardImg = document.querySelectorAll('.cardImg')
     fetch(url)
     .then(response => response.json())
+    
 
     .then(data => {
         data.forEach((element,index) => {
             if(index<5)
-             console.log(element.title)
-            
-         });
-     }
-        )
+            //console.log(element)
+            //console.log(cardImg[i])
+             cardImg[i].innerHTML = `<div class="thumbnail">
+            <img src="${element.thumbnailUrl}" alt="">
+            <div class="caption">
+              <p>${element.title} </p>
+            </div>
+          </div>`
+            i++ ;
+        })
+    })
+       // console.log(arr)
